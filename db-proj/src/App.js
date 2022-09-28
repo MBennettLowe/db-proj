e391d02c29f3a54cc3ab92950d10dd28ef54e5e1
MBennettLowe <mbennettlowe@gmail.com>
2022-09-28 18:44:35 -0400

    CSS Added

--- db-proj/src/App.js
+++ db-proj/src/App.js
@@ -1,12 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
 
+// import Records from './records.json'
+import Data from './data.json'
+
 function App() {
-  return (
+  return(
     <div>
-      <h1>Hello from React</h1>
-    </div>
-  );
+      <h2>JSON Data</h2>
+
+{
+  Data.map( data => {
+    return(
+      <div className='data-text' key={data.id}>
+        <li className='data-li'>{data.title}</li>
+       {data.content}
+      </div>
+    )
+  })
 }
 
+    </div>
+  )
+}
 export default App;
